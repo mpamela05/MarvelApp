3a8e3727228db389d3b8bdd156d5d1ed37540274
mpamela05 <pamelamandic46@gmail.com>
2025-03-13 17:08:31 +0100

    Add project files.

---
+++ MarvelApp/Models/Identitet.cs
@@ -0,0 +1,13 @@
+﻿namespace MarvelApp.Models
+{
+    public class Identitet
+    {
+        public string Ime { get; set; }
+        public string? Prezime { get; set; }
+        public int? Godine { get; set; }
+        public int? God_rodjenja { get; set; }
+        public int? God_smrti { get; set; }
+
+
+    }
+}
